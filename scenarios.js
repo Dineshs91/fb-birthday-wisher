060e98d9ac26ac2c17f16152d1f00fcebc2588de
dinesh <dineshpy07@gmail.com>
2015-11-28 22:25:45 +0530

    Inception

---
+++ scenarios.js
@@ -0,0 +1,45 @@
+var setup = require('./setup.js')
+
+var fb_email = process.env.FB_EMAIL;
+var fb_pass = process.env.FB_PASS;
+
+var wishes = [
+    'Happy Birthday !!',
+    'Happy Birthday! Have a blast.',
+    'Many more happy returns of the day'
+];
+
+var getRandomWish = function() {
+    var min = 0;
+    var max = wishes.length;
+
+    var index = Math.floor(Math.random() * (max - min) + min);
+    return wishes[index];
+};
+
+setup.start();
+
+var webdriverio = require('webdriverio'),
+    client = webdriverio.remote({
+        desiredCapabilities: {
+            browserName: 'phantomjs',
+            'phantomjs.binary.path': require('phantomjs').path,
+            resolution : '1024x768'
+        },
+        logLevel: 'verbose',
+    }).init();
+
+client
+    .url('https://facebook.com')
+    .setValue('#email', fb_email)
+    .setValue('#pass', fb_pass)
+    .click('#loginbutton')
+    .pause(1000)
+    .click('.fbRemindersTitle')
+    .pause(1000)
+    .setValue('[name="message_text"]', getRandomWish())
+    .keys('Enter')
+    .pause(500)
+    .end().then(function() {
+        setup.exit();
+    });
---
+++ setup.js
@@ -0,0 +1,23 @@
+var childProcess = require('child_process');
+var http = require('http');
+
+var filename = 'selenium';
+var args = ['-jar', filename];
+var seleniumProcess;
+
+var start = function() {
+    seleniumProcess = childProcess.spawn('java', args, {
+        stdio: 'ignore',
+        detached: true
+    });
+};
+
+var exit = function() {
+    console.log('Operation complete -- Exiting now...');
+    http.get('http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer');
+};
+
+module.exports = {
+    start,
+    exit
+};
