8ef7d38c1ff0b5daafa2dea7a58fe33a55138965
dinesh <dineshpy07@gmail.com>
2015-11-29 22:11:48 +0530

    Modify child process code

--- setup.js
+++ setup.js
@@ -7,8 +7,18 @@ var seleniumProcess;
 
 var start = function() {
     seleniumProcess = childProcess.spawn('java', args, {
-        stdio: 'ignore',
-        detached: true
+        stdio: 'inherit',
+    });
+
+    seleniumProcess.on('exit', function(code) {
+        console.log('Selenium Standalone has exited with code ' + code);
+        process.exit(code);
+    });
+
+    process.stdin.resume();
+    process.stdin.on('data', function(chunk) {
+        console.log('Attempting to shut down selenium nicely');
+        http.get('http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer');
     });
 
     seleniumProcess.on('error', function(err) {
@@ -18,7 +28,11 @@ var start = function() {
 
 var exit = function() {
     console.log('Operation complete -- Exiting now...');
-    http.get('http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer');
+    try {
+        http.get('http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer');
+    } catch(err) {
+        console.err(err);
+    }
 };
 
 module.exports = {
